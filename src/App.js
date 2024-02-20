6884f45a4b563c1e80136715e741bc2ffb9a3aa3
M-Ehsan-Ali <mrehsa51@gmail.com>
2024-02-20 18:58:54 +0500

    initial commits

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import "./App.css";
+import About from "./Components/About";
+import Home from "./Components/Home";
+import Users from "./Components/Users";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar bg="primary" data-bs-theme="dark">
+          <Container>
+            <Navbar.Brand href="/">Navbar</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link>
+                <Link to="/">Home</Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link to="about">About</Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link to="users">Users</Link>
+              </Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+        <Routes>
+          <Route path="/" Component={Home} />
+          <Route path="/about" Component={About} />
+          <Route path="/users" Component={Users} />
+        </Routes>
+      </Router>
     </div>
   );
 }
