6d773442508aa8daed9c10face4c3a01f830a7c3
M-Ehsan-Ali <mrehsa51@gmail.com>
2024-02-26 17:03:13 +0500

    secure port added for notification

--- src/service-worker-dev.js
+++ src/service-worker-dev.js
@@ -2,7 +2,7 @@ export default function serviceWorkerDev() {
   function urlBaseToUnit8Array(base64String) {
     const padding = "=".repeat(4 - ((base64String.length % 4) % 4));
     const base64 = (base64String + padding)
-      .replace(/\-/g, "+")
+      .replace(/-/g, "+")
       .replace(/_/g, "/");
     const rawData = window.atob(base64);
     const outputArray = new Uint8Array(rawData.length);
